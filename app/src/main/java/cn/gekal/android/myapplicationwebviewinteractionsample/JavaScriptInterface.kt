a213b805c655dbf5c7117fb30fc1e907c47b3af1
gekal <liu.hongying@hotmail.com>
2024-11-12 01:13:49 +0900

    finish sample

--- app/src/main/java/cn/gekal/android/myapplicationwebviewinteractionsample/JavaScriptInterface.kt
+++ app/src/main/java/cn/gekal/android/myapplicationwebviewinteractionsample/JavaScriptInterface.kt
@@ -1,12 +1,19 @@
 package cn.gekal.android.myapplicationwebviewinteractionsample
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.webkit.JavascriptInterface
+import android.webkit.WebView
 import android.widget.Toast
 
-class JavaScriptInterface(private val context: Context) {
+class JavaScriptInterface(private val context: Context, private val webView: WebView) {
     @JavascriptInterface
     fun showToast(message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+
+        Handler(Looper.getMainLooper()).post {
+            webView.evaluateJavascript("javascript: handleReturnValue('Hello from Android!')", null)
+        }
     }
 }
--- app/src/main/java/cn/gekal/android/myapplicationwebviewinteractionsample/MainActivity.kt
+++ app/src/main/java/cn/gekal/android/myapplicationwebviewinteractionsample/MainActivity.kt
@@ -1,5 +1,6 @@
 package cn.gekal.android.myapplicationwebviewinteractionsample
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
@@ -10,6 +11,7 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var webView: WebView
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,10 +20,7 @@ class MainActivity : ComponentActivity() {
         webView.settings.cacheMode = WebSettings.LOAD_NO_CACHE
         webView.settings.javaScriptEnabled = true
         webView.webViewClient = WebViewClient()
-        webView.addJavascriptInterface(JavaScriptInterface(this), "AndroidInterface")
+        webView.addJavascriptInterface(JavaScriptInterface(this, webView), "AndroidInterface")
         webView.loadUrl("https://gekal-study-android.github.io/webview-interaction-sample/index.html")
-
-        // WebView内でJavaScriptの関数を呼び出す
-        webView.evaluateJavascript("javascript: handleReturnValue('Hello from Android!')", null)
     }
 }
