c931e9e706106dbd82e53b787bacd7734954eddb
gekal <liu.hongying@hotmail.com>
2025-05-10 07:05:41 +0900

    Add Firebase BOM and Google Services plugin support
    Integrated Firebase BOM for dependency management and added Google Services plugin configuration to the build setup. Updated `.gitignore` to exclude `google-services.json` and adjusted formatting in configuration files for better consistency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
-import com.android.build.gradle.internal.scope.ProjectInfo.Companion.getBaseName
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.spotless) apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.kotlin.compose) apply false
+  alias(libs.plugins.spotless) apply false
+  alias(libs.plugins.google.services) apply false
 }
 
 subprojects {
-    plugins.apply("com.diffplug.spotless")
+  plugins.apply("com.diffplug.spotless")
 }
